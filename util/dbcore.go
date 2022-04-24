83f6ddb499e116547c3b2f58ad185e65840bdc2a
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-04-24 10:48:02 +0800

    fix：修改数据库链接

--- util/dbcore.go
+++ util/dbcore.go
@@ -11,9 +11,23 @@ import (
 var DB *gorm.DB
 var err error
 
+type Sql struct {
+	root     string
+	password string
+	local    string
+	sqlname  string
+}
+
 func init() {
+	getsql := Sql{
+		root:     "root",
+		password: "123456",
+		local:    "127.0.0.1:3306",
+		sqlname:  "vueparent",
+	}
+
 	// 用户名:密码@tcp(ip:port)/数据库?charset=utf8mb4&parseTime=True&loc=Local
-	dsn := "root:123456@tcp(127.0.0.1:3306)/vueparent?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := getsql.root + ":" + getsql.password + "@tcp(" + getsql.local + ")/" + getsql.sqlname + "?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true, // 使用单数表名
