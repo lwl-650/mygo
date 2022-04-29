b1cf2e6ee5f8d1ff66d85450d3872a3b52e8e5aa
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-04-29 18:23:19 +0800

    fix: ok

--- controller/apis/adminController.go
+++ controller/apis/adminController.go
@@ -36,21 +36,12 @@ func (AdminController) FindAdmin(c *gin.Context) {
 }
 
 func (AdminController) UpdateAdmin(c *gin.Context) {
-	// A_name := c.PostForm("aname")
-	// A_pass := c.PostForm("apass")
-	// A_portrait := util.If(c.PostForm("aportrait") == "",
-	// "http://q.vuetitle.lwlsl.top/title.png", c.PostForm("aportrait"))
-	// A_grade, _ := strconv.Atoi(c.PostForm("agrade"))
-	// setAdmin := model.Admin{
-	// 	A_name:     A_name,
-	// 	A_pass:     A_pass,
-	// 	A_portrait: A_portrait,
-	// 	A_grade:    A_grade,
-	// }
-	// fmt.Println(setAdmin)
+	A_name := c.PostForm("aname")
+	A_pass := c.PostForm("apass")
+	A_portrait := util.If(c.PostForm("aportrait") == "",
+		"http://q.vuetitle.lwlsl.top/title.png", c.PostForm("aportrait"))
+	A_grade, _ := strconv.Atoi(c.PostForm("agrade"))
 	setAdmin := model.Admin{}
-	// util.DB.Create(&setAdmin)
-	// util.DB.Model(&setAdmin).Update("a_name", "啦啦啦")
 	util.DB.Model(&setAdmin).Where("a_id = ?", 4).Updates(map[string]interface{}{
-		"a_name": "world", "a_pass": "18", "a_portrait": "5165", "a_grade": 2})
+		"a_name": A_name, "a_pass": A_pass, "a_portrait": A_portrait, "a_grade": A_grade})
 }
